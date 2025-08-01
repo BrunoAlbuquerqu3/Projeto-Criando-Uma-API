2a186c9ccb437db727e7cc9d9977a02504a9c367
Bruno Albuquerque <126733239+BrunoAlbuquerqu3@users.noreply.github.com>
2025-08-01 12:11:57 -0300

    Validando Administrador com login e senha

--- Infraestrutura/db/DbContexto.cs
+++ Infraestrutura/db/DbContexto.cs
@@ -1,6 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
+using Microsoft.Extensions.Configuration;
+
 namespace MinimalApi.Infraestrutura.Db;
 
-public class DbContexto
+public class DbContexto : DbContext
 {
-    
+    private readonly IConfiguration _configuracaoAppSettings;
+    public DbContexto(IConfiguration configuracaoAppSettings)
+    {
+        _configuracaoAppSettings = configuracaoAppSettings; 
+    }
+    public DbSet<Administrador> Administradores { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Administrador>().HasData(
+            new Administrador
+            {
+                Id = -1,
+                Email = "Administrador@teste.com",
+                Senha = "123456",
+                Perfil = "Adm"
+            }
+        );
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+
+            var StringConexao = _configuracaoAppSettings.GetConnectionString("mysql")?.ToString();
+            if (!string.IsNullOrEmpty(StringConexao))
+            {
+                optionsBuilder.UseMySql(
+                    StringConexao,
+                    ServerVersion.AutoDetect(StringConexao)
+                );
+            }
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,15 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Interfaces;
+using MinimalApi.Dominio.Servicos;
+using MinimalApi.DTOs;
+using MinimalApi.Infraestrutura.Db; // Para a sua classe DbContexto
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+
+//builder.Services.AddScoped<DbContext
+
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Olá Pessoal!");
 
-app.MapPost("/login",(MinimalApi.DTOs.LoginDTO LoginDTO) => {
-    if (LoginDTO.Email == "adm@teste.com" && LoginDTO.Senha == "123456")
-
+app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico ) => {
+    if (administradorServico.Login(loginDTO) != null)
         return Results.Ok("Login com sucesso");
     else
-
         return Results.Unauthorized(); 
-    });
+});
 app.Run();
 
